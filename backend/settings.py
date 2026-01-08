b03cdf23577db75f7819bbdc18062bae223abf74
bonyaminshaznuz <mdshaznuz@gmail.com>
2026-01-08 13:45:08 +0100

    Deployment Update 3

--- backend/settings.py
+++ backend/settings.py
@@ -155,14 +155,14 @@ else:
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'mdshaznuz@gmail.com'
-EMAIL_HOST_PASSWORD = 'grev dkvd kjub ttzd'
-DEFAULT_FROM_EMAIL = 'Dr. Abul Khayer (Biplob) <mdshaznuz@gmail.com>'
-ADMIN_EMAIL = 'mdshaznuz@gmail.com'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', f"Dr. Abul Khayer (Biplob) <{EMAIL_HOST_USER}>")
+ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', EMAIL_HOST_USER)
 
 LOGGING = {
     'version': 1,
