5e3959013c786cb867b4e2e8cb83218166dcb1e1
bonyaminshaznuz <mdshaznuz@gmail.com>
2026-01-10 18:47:12 +0100

    image fix 2

--- backend/urls.py
+++ backend/urls.py
@@ -15,17 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Appointment.urls')),
     path('', include('Portfolio.urls')),
-]
-
-# Serve media files (both development and production)
-# In production, you should ideally use nginx/apache to serve media files
-# but this will work for now
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Serve media files in production
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # Serve static files in production
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
