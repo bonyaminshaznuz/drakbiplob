f82c51302aab517c019c3ce7f1a82ab7ebac97d3
bonyaminshaznuz <mdshaznuz@gmail.com>
2026-01-11 16:01:15 +0100

    update database username

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Create superuser if not exists (username: admin, password: pass)
+# Create superuser if not exists (username: akbiplob@, password: qQpR28a2gVJmGWY)
 python manage.py shell << END
 from django.contrib.auth import get_user_model; User = get_user_model();
-if not User.objects.filter(username='admin').exists():
+if not User.objects.filter(username='akbiplob@').exists():
 	User.objects.create_superuser('akbiplob@', '', 'qQpR28a2gVJmGWY')
 END
